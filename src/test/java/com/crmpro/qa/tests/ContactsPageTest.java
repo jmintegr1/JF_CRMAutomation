a0c6508deedd6bd1f66f2cc48fe5e4f16a77e0ef
jmintegr1 <jmintegr1@gmail.com>
2019-02-25 20:29:40 -0500

    Updated ContactPageTest

--- src/test/java/com/crmpro/qa/tests/ContactsPageTest.java
+++ src/test/java/com/crmpro/qa/tests/ContactsPageTest.java
@@ -1,11 +1,19 @@
+/*
+ * 
+ * @Author: JewellM
+ * 
+ */
+
+
+
+
+
 package com.crmpro.qa.tests;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.Test;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.DataProvider;
 import org.testng.Assert;
-
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import com.crm.qa.base.TestBase;
 import com.crm.qa.pages.ContactsPage;
