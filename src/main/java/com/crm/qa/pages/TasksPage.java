eb7bacfa3467be38ded79904367942ef80d79c58
jmintegr1 <jmintegr1@gmail.com>
2019-09-11 08:39:11 -0400

    Notes below IBman

--- src/main/java/com/crm/qa/pages/TasksPage.java
+++ src/main/java/com/crm/qa/pages/TasksPage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class TasksPage extends PageObjectBase {  //Ibrahim's example of how real world environment does it:  as they use two different pages like TestBase or  
-	                                        //My daily routine will be more intense in learning and in fitness!!
+	                                        //My daily routine will be more intense in learning and in fitness! It's time to go to the next level of my evolution. 
 	public TasksPage(WebDriver driver) {
 		super(driver);
 	}
